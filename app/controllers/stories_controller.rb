300ef385a673070a67bbbdbc1faf381974371ab1
Arena <arena@visualarena.com>
2015-05-29 15:39:09 -0700

    modifications to the stories page
    this is getting there, doesn’t work locally so I’m deploying mid way
    through to test.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,7 +5,8 @@ class StoriesController < ApplicationController
   def index
     all_stories = EmergencyStory.order("index,id ASC").all
     @selected_story = all_stories.select {|s| s.selected}.first
-    @stories = all_stories.collect.select {|s| !s.selected}
+    @stories = all_stories.collect.select {|s| !s.selected}.slice(0,2)
+    # to show more stories take the slice off the end of the line above -arena
   end
 
 end
